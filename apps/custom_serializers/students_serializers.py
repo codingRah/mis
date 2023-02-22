43de5f69284cb3213a4ff50ee96af769f504be0e
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-02-22 16:36:51 +0430

    student serializer created

--- apps/custom_serializers/students_serializers.py
+++ apps/custom_serializers/students_serializers.py
@@ -15,5 +15,19 @@ class StudentStatusSerializer(serializers.ModelSerializer):
 
 
 class StudentsCartSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model  = StudentNationlityCartInfo
+        fields = ['id','student','cart_type','cart_id','page_no','register_no','volume_no','front_image','back_image']
+    
+class StudentHostelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentHostelService
+        fields = ['id','student','service_type','wing_no','room_no']
+
+
+class StudentRelationContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentRelationContact
+        fields = ['id','student','relation','relative_name','occupation','phone1','phone2']
+
 
