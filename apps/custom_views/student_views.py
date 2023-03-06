51535be201b63a7e8753f6bc7ba33e3697ef4236
NaimatullahRezaee <naimatullahrezaee@gmail.com>
2023-03-06 14:08:38 +0430

    changes in studen view

--- apps/custom_views/student_views.py
+++ apps/custom_views/student_views.py
@@ -17,7 +17,7 @@ class StudentViews(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentsSerializer
     # permission_classes =  [IsAuthenticated,]
-    # pagination_class = StudentPagination
+    pagination_class = StudentPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, OrderingFilter]
     filterset_class = StudentFilter
     search_fields = ["kankor_id","first_name","last_name"]
